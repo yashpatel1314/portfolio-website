c9b1425734d510a81ba60575598d92d36bfd9b24
yashpatel1314 <yashpatel1314@outlook.com>
2024-06-05 12:48:03 -0400

    Final Changes (20240605)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import yash from "../photos/profile/photo-masthead-375-b8ae1548 (1).webp"
+import yash from "../photos/profile/titlepic.jpg"
 import '../css/general.css';
 import '../css/action.css';
 import '../css/home.css';
@@ -7,13 +7,13 @@ export default function Home() {
     return(
         <div className="homeBackground">
             <div className="row">
-                <div className="column">
+                <div className="homecolumn">
                     <div className="title-box">
                         <h1>Hello, My Name is Yash Patel!</h1>
                         <h3>A 2nd Year Undergraduate Student @ The University of Waterloo</h3>
                     </div>
                 </div>
-                <div className="column">
+                <div className="homecolumn">
                     <div className="title-picture">
                         <img className="profile-pic" src={yash} alt="yash" />
                     </div>
